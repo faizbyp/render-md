0e77738c9fa569b72eefca6076e68261d605a557
faizbyp <faizbyp@gmail.com>
2024-05-29 13:14:28 +0000

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import DOMPurify from "dompurify";
 import { marked } from "marked";
 
 function App() {
-  const str = DOMPurify.sanitize(marked.parse('# Test\n`test code`'));
+  const current = '# Test\n`test code`';
+
+  const str = DOMPurify.sanitize(marked.parse(current));
   const md = { __html: str };
 
   return (
-    <>
-      <h1>Render.md</h1>
-      <div dangerouslySetInnerHTML={md} />
-    </>
+    <main className="container-fluid" style={{height: '100svh'}}>
+      <section className="row text-center">
+        <h1>Render.md</h1>
+      </section>
+      <div className="row">
+        <section className="col-12 col-lg-6">
+          <textarea name="markdown" className="form-control" id="text" cols="30" rows="10">
+            {current}
+          </textarea>
+        </section>
+        <section className="col-12 col-lg-6">
+          <div dangerouslySetInnerHTML={md} />
+        </section>
+      </div>
+    </main>
   );
 }
 
