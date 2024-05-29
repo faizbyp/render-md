6af5be4571bd4e2fd106b852f2948a1dc5756ad3
faizbyp <faizbyp@gmail.com>
2024-05-29 17:16:20 +0700

    parse md to html work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { marked } from "marked";
+
 function App() {
+  const str = marked.parse('# Test')
+
   return (
     <>
       <h1>Render.md</h1>
+      <div>
+        {str}
+      </div>
     </>
   );
 }
