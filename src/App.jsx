42fb1aad05a8134f55d1f90d773d696e904b91bb
faizbyp <faizbyp@gmail.com>
2024-05-31 23:33:39 +0700

    tidying code

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { marked } from "marked";
 import { initial } from "./initial";
 import { useState } from "react";
 import { Popover } from "react-tiny-popover";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   marked.setOptions({
-    breaks: true
-  })
+    breaks: true,
+  });
 
   const [text, setText] = useState(initial);
   const [isCopied, setIsCopied] = useState(false);
@@ -18,35 +18,48 @@ function App() {
   const handleCopy = () => {
     navigator.clipboard.writeText(text);
     setIsCopied(true);
-  }
+  };
 
   return (
     <>
-    <main className="container" style={{height: '100svh'}}>
-      <section className="row text-center py-2">
-        <h1 className="text-body-secondary">Render.md</h1>
-      </section>
-      <div className="row">
-        <section className="col-12 col-lg-6">
-          <p className="text-body-tertiary">{`// Input`}</p>
-          <textarea value={text} className="form-control mb-3" id="editor" rows="15" onChange={e => setText(e.target.value)} />
+      <main className="container" style={{ height: "100svh" }}>
+        <section className="row text-center py-2">
+          <h1 className="text-body-secondary">Render.md</h1>
         </section>
-        <section className="col-12 col-lg-6 overflow-x-hidden">
-          <div className="row">
-            <div className="col">
-              <p className="text-body-tertiary">{`// Output`}</p>
+        <div className="row">
+          <section className="col-12 col-lg-6">
+            <p className="text-body-tertiary">{`// Input`}</p>
+            <textarea
+              value={text}
+              className="form-control mb-3"
+              id="editor"
+              rows="15"
+              onChange={(e) => setText(e.target.value)}
+            />
+          </section>
+          <section className="col-12 col-lg-6 overflow-x-hidden">
+            <div className="row">
+              <div className="col">
+                <p className="text-body-tertiary">{`// Output`}</p>
+              </div>
+              <div className="col text-end">
+                <Popover
+                  isOpen={isCopied}
+                  positions={["top", "left"]}
+                  onClickOutside={() => setIsCopied(false)}
+                  content={<p className="text-warning">Copied!</p>}
+                >
+                  <button className="btn btn-secondary me-2" onClick={handleCopy}>
+                    Copy
+                  </button>
+                </Popover>
+              </div>
             </div>
-            <div className="col text-end">
-              <Popover isOpen={isCopied} positions={["top", "left"]} onClickOutside={() => setIsCopied(false)} content={<p className="text-warning">Copied!</p>}>
-                <button className="btn btn-secondary me-2" onClick={handleCopy}>Copy</button>
-              </Popover>
-            </div>
-          </div>
-          <div id="preview" dangerouslySetInnerHTML={md} />
-        </section>
-      </div>
-    </main>
-    <Analytics />
+            <div id="preview" dangerouslySetInnerHTML={md} />
+          </section>
+        </div>
+      </main>
+      <Analytics />
     </>
   );
 }
